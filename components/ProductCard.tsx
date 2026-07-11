6603395a39da29a427435f9ea7b528a70a407ca6
Teodoro Charles <108039629+tdacharles4@users.noreply.github.com>
2026-07-10 17:30:27 -0700

    card pt1 from windows

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -2,8 +2,22 @@
 
 import * as React from "react";
 import Image from "next/image";
+import { ShopifyProduct } from "@/lib/shopify";
 
 export function ProductCard(){
+
+    /*
+    const [isCartHovered, setIsCartHovered] = React.useState(false);
+    const image = product.images.edges[0]?.node;
+
+    const formatPrice = (price: { amount: string; currencyCode: string }) => 
+    new Intl.NumberFormat("es-MX", {
+      style: "currency",
+      currency: price.currencyCode,
+    }).format(parseFloat(price.amount));
+
+    const isOutOfStock = !product.availableForSale;
+    */
 
     return(
         <>
